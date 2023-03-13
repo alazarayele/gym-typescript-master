a0bafeb802c68203f3bb7c31d3238378a103e138
alazarayele <alazarayele47@gmail.com>
2023-03-13 10:20:04 +0300

    first commit

---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,98 @@
+import { SelectedPage, ClassType } from "@/shared/types";
+import image7 from "@/assets/image7.jpg";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+import Bicycle from "@/assets/Bicycle.png";
+import GYMRUN from "@/assets/GYMRUN.jpg"
+import GYMSRC from "@/assets/GYMSRC.jpg"
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
+import Class from "./Class";
+
+const classes: Array<ClassType> = [
+  {
+    name: "Weight Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: Bicycle,
+  },
+  {
+    name: "Yoga Classes",
+    image: GYMRUN,
+  },
+  {
+    name: "Ab Core Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: GYMSRC,
+  },
+  {
+    name: "Adventure Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: Bicycle,
+  },
+  {
+    name: "Fitness Classes",
+    image: Bicycle,
+  },
+  {
+    name: "Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: Bicycle,
+  },
+];
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="ourclasses" className="w-full bg-Green-Top py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+              tellus quam porttitor. Mauris velit euismod elementum arcu neque
+              facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+              enim mattis odio in risus nunc.
+            </p>
+          </div>
+        </motion.div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item: ClassType, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default OurClasses;
